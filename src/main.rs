7c4af64beb52a7af1d7f0351bfd457d6468be574
Mari <me@mari.zip>
2026-04-25 12:06:41 +0000

    add auth

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env;
 use std::sync::Arc;
 use anyhow::{Context, Result};
 use poem::listener::TcpListener;
@@ -7,14 +8,18 @@ use crate::config::{Item, load_config};
 use crate::routes::all_routes;
 
 mod config;
+mod middleware;
 mod routes;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let config: Arc<HashMap<String, Vec<Item>>> = Arc::new(load_config().await?);
-    let port = std::env::var("PORT").unwrap_or_else(|_| "7123".to_string());
+    let username = env::var("PALM_USERNAME").context("could not find env variable `PALM_USERNAME`")?;
+    let password = env::var("PALM_PASSWORD").context("could not find env variable `PALM_PASSWORD`")?;
+
+    let port = env::var("PORT").unwrap_or_else(|_| "7123".to_string());
     let address = format!("0.0.0.0:{port}");
 
     println!("starting server on {address}");
-    Server::new(TcpListener::bind(address)).run(all_routes(config)).await.context("failed to start server")
+    Server::new(TcpListener::bind(address)).run(all_routes(config, username, password)).await.context("failed to start server")
 }
--- src/routes.rs
+++ src/routes.rs
@@ -5,12 +5,14 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use poem::{EndpointExt, Route};
 use crate::config::Item;
+use crate::middleware::BasicAuth;
 use crate::routes::api::create_transaction;
 use crate::routes::frontend::index;
 
-pub(crate) fn all_routes(config: Arc<HashMap<String, Vec<Item>>>) -> impl poem::Endpoint {
+pub(crate) fn all_routes(config: Arc<HashMap<String, Vec<Item>>>, username: String, password: String) -> impl poem::Endpoint {
     Route::new()
         .at("/", index)
         .at("/api/create/:id", create_transaction)
         .data(config)
+        .with(BasicAuth::new(username, password))
 }
